6dd7d3dbe57581827032eb5285e59695acb723ff
Hugo <hugo.chea@cintel.fr>
2022-04-29 11:30:14 +0200

    initialize front

--- job-booking-client/src/app/app-routing.module.ts
+++ job-booking-client/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('@modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'authentication',
+    loadChildren: () =>
+      import('@modules/authentication/authentication.module').then(m => m.AuthenticationModule)
+  },
+  {
+    path: 'recruiter',
+    loadChildren: () =>
+      import('@modules/recruiter/recruiter.module').then(m => m.RecruiterModule)
+  },
+  {
+    path: 'authentication',
+    loadChildren: () =>
+      import('@modules/applicant/applicant.module').then(m => m.ApplicantModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
